873801e8678012d7c316cff7afa662b421c1a8a2
irfanmkh <irfankhakiki17@gmail.com>
2026-07-22 06:41:21 +0700

    chore: setup api routes and install laravel sanctum

---
+++ hr-backend/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\api\ApplicationController;
+use App\Http\Controllers\api\CandidateController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\JobListController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/jobs', [JobListController::class, 'index']);
+Route::post('/jobs', [JobListController::class, 'store']);
+Route::put('/jobs/{id}', [JobListController::class, 'update']);
+Route::delete('/jobs/{id}', [JobListController::class, 'destroy']);
+
+Route::get('/candidates', [CandidateController::class, 'index']);
+Route::post('/candidates', [CandidateController::class, 'store']);
+Route::put('/candidates/{id}', [CandidateController::class, 'update']);
+Route::get('/candidates/{id}', [CandidateController::class, 'show']);
+
+Route::get('/applications', [ApplicationController::class, 'index']);
+Route::post('/applications', [ApplicationController::class, 'store']);
