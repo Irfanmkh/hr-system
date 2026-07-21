9b86c0b15260e52047e7bfc7034228b8cec6889a
irfanmkh <irfankhakiki17@gmail.com>
2026-07-22 06:39:24 +0700

    feat : add JobList controller & resource untuk fitur manajemen daftar lowongan kerja

---
+++ hr-backend/app/Http/Controllers/api/JobListController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\JobListResource;
+use Illuminate\Http\Request;
+use App\Models\JobList;
+
+class JobListController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $query = JobList::query();
+
+        // search
+
+        if ($request->filled('search')) {
+            $search = strtolower(trim($request->search));
+            $query->where(function ($q) use ($search) {
+                $q->whereRaw('LOWER(title) LIKE ?', ["%{$search}%"])
+                  ->orWhereRaw('LOWER(department) LIKE ?', ["%{$search}%"]);
+
+            });
+
+        }
+
+        // filter status (is_active)
+        if ($request->has('is_active') && $request->input('is_active') !== null && $request->input('is_active') !== '') {
+            $isActive = $request->boolean('is_active');
+            $query->where('is_active', $isActive);
+        }
+
+        // sort
+        $sortBy = $request->query('sort_by', 'created_at');
+        $sortDir = strtolower($request->query('sort_dir', 'desc')) === 'asc' ? 'asc' : 'desc';
+
+        // validasi sql inject
+        $allowedSorts = ['title', 'department', 'is_active', 'created_at'];
+        if (in_array($sortBy, $allowedSorts)) {
+            $query->orderBy($sortBy, $sortDir);
+        } else {
+            $query->latest();
+        }
+
+        // paginasi
+        $perPage = $request->get('per_page', 10);
+
+        $jobs = $query->paginate($perPage);
+
+
+        return JobListResource::collection($jobs);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title'       => 'required|string|max:255',
+            'department'  => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_active'   => 'boolean',
+        ]);
+
+        $job = JobList::create($validated);
+
+        return new JobListResource($job);
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+
+        $joblist = JobList::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'sometimes|required|string',
+            'department' => 'sometimes|required|string',
+            'description' => 'nullable|string',
+            'is_active' => 'boolean'
+        ]);
+
+        $joblist->update($validated);
+
+        return new JobListResource($joblist);
+
+    }
+
+
+    public function destroy($id)
+    {
+        $joblist = JobList::findOrFail($id);
+
+        $joblist->delete();
+        return response()->json([
+            'message' => 'lowongan kerja berhasil dihapus!'
+        ], 200);
+    }
+}
